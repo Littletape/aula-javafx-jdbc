6484003735a1bf76057ab2be409305fbbf4d8f20
Littletape <andersoncardosolima0@gmail.com>
2020-06-15 21:42:14 -0300

    DepartmentList controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable {
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+
+	@FXML
+	private TableColumn<Department, Integer> tableColumId;
+
+	@FXML
+	private TableColumn<Department, String> tableColumName;
+
+	@FXML
+	private Button btNew;
+
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		// prepara as celulas da tablela para receber seus respectivos valores.
+		tableColumId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+		// ajusta o tamanho da tabela para o tamanho da janela
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+}
